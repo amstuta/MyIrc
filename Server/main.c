2e2b4c51a3b57ccf01542eab7d534741976d9f03
amstut_a <arthur.amstutz@gmail.com>
2015-03-27 14:06:57 +0100

    c est super

--- Server/main.c
+++ Server/main.c
@@ -5,10 +5,58 @@
 ** Login   <amstuta@epitech.net>
 **
 ** Started on  Fri Mar 27 13:08:12 2015 arthur
-** Last update Fri Mar 27 13:08:21 2015 arthur
+** Last update Fri Mar 27 13:31:56 2015 arthur
 */
 
-int	main()
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+void			accept_client(int fd, int cs, int port)
 {
+  struct sockaddr_in	sin_c;
+  int			c_len;
+
+  c_len = sizeof(sin_c);
+  while (1)
+    {
+      cs = accept(fd, (struct sockaddr*)&sin_c, (socklen_t*)&c_len);
+      if (cs == -1)
+	return ;
+      g_fd = cs;
+    }
+}
+
+void			create_socket(int port)
+{
+  int			cs;
+  int			fd;
+  struct protoent	*pe;
+  struct sockaddr_in	sin;
+
+  pe = getprotobyname("TCP");
+  if ((fd = socket(AF_INET, SOCK_STREAM, pe->p_proto)) == -1)
+    return ;
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(port);
+  sin.sin_addr.s_addr = INADDR_ANY;
+  if (bind(fd, (const struct sockaddr*)&sin, sizeof(sin)) == -1)
+    {
+      close(fd);
+      return ;
+    }
+  if ((cs = listen(fd, 1)) == -1)
+    {
+      close(fd);
+      return ;
+    }
+  accept_clients(fd, cs, port);
+}
+
+int			main()
+{ 
   return (0);
 }
